ebe2a994c676b1e0d1093160a275411e30d396a6
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-28 12:03:43 -0800

    Added print_piece to header

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/28 10:39:11 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/02/28 10:42:12 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/02/28 12:03:22 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 # include <stdint.h>
 # include <unistd.h>
+
+void	print_piece(uint16_t piece, char c);
 
 #endif

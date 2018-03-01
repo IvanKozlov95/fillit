e8bdec70e446595f42d4a2996eff5e06ae504197
Ivan Kozlov <ikozlov9000@gmail.com>
2018-02-28 17:33:14 -0800

    Added check_piece and place_piece functions

--- src/solver.c
+++ src/solver.c
@@ -1,4 +1,35 @@
-int			solve(t_etris *tetris, const int count)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solver.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/28 16:57:25 by ikozlov           #+#    #+#             */
+/*   Updated: 2018/02/28 17:32:31 by ikozlov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+/*
+** Checks if piece can be placed on the map
+*/
+
+static int	check_piece(t_piece piece, uint16_t *map)
+{
+	return (!((*(uint64_t *)(map + piece.y) & (piece.value >> piece.x))));
+}
+
+/*
+** Puts piece on the map
+*/
+
+static void	place_piece(t_piece piece, uint16_t *map)
+{
+	*(uint64_t *)(map + piece.y) ^= piece.value >> piece.x;
+}
+
 {
 	uint16_t	map[MAX_MAP]];
 	int			size;
